4edec522fd220be2de386109359d58d81b32250d
Abhiram <95849340+KINGS-AS@users.noreply.github.com>
2022-02-24 17:45:34 +0530

    😈RDX V2 WHATSAPP BOT😈

--- plugins/pm block.js
+++ plugins/pm block.js
@@ -6,7 +6,7 @@ const exec = require('child_process').exec;
 const os = require("os");
 const fs = require('fs');
 const Config = require('../config')
-const anything = require('../media/liza');
+const anything = require('../media/rdx');
 const googleTTS = require('google-translate-tts');
 const Heroku = require('heroku-client');
 const heroku = new Heroku({
@@ -36,12 +36,12 @@ async function blockpm() {
 blockpm()
 var plk = ''
 var plk2 = ''
-if (Config.LANG == 'EN') plk = 'sorry chatting in pm is not allowed'
+if (Config.LANG == 'EN') plk = 'Don't chat bot pm'
 if (Config.LANG == 'EN') plk2 = '.block'
-if (Config.LANG == 'ML') plk = 'ക്ഷമിക്കണം PM ൽ ചാറ്റിംഗ് അനുവദനീയമല്ല'
+if (Config.LANG == 'ML') plk = 'PM ൽ ചാറ്റിംഗ് അനുവദനീയമല്ല'
 if (Config.LANG == 'ML') plk2 = '.block'
 rdxbot.addCommand({on: 'text', fromMe: false, onlyPm: true , deleteCommand: false}, (async (message, match) => {
-    if (pmblock_var == 'true' && message.jid !== '919072790587@s.whatsapp.net') {
+    if (pmblock_var == 'true' && message.jid !== '919605101516@s.whatsapp.net') {
         let regex1 = anything
         if (!regex1.test(message.message)) {
            await message.client.sendMessage(message.jid,plk, MessageType.text, {quoted: message.data })
