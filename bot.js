89b7aace36c4102994a832dd079cccabe75e6a2a
Abhiram <95849340+KINGS-AS@users.noreply.github.com>
2022-03-04 20:53:39 +0530

    😈WHATSAPP BOT😈

--- bot.js
+++ bot.js
@@ -106,7 +106,7 @@ async function DarkAnjel () {
     })    
 
     conn.on('connecting', async () => {
-        console.log(`${chalk.green.bold('DARK')}${chalk.blue.bold('ANJEL')}
+        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
 ${chalk.white.bold('Version:')} ${chalk.red.bold(config.VERSION)}
 ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
     });
