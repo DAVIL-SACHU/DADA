61e43632295e4fcfeb0dd3832f4815d8d096b6df
Abhiram <95849340+KINGS-AS@users.noreply.github.com>
2022-03-15 08:16:10 +0530

    😈WHATSAPP BOT😈

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ const Language = require('./language');
 const Lang = Language.getString('updater');
 
 // Sql
-const WhatsAsenaDB = config.DATABASE.define('WhatsAsena', {
+const DadaDB = config.DATABASE.define('Dada', {
     info: {
       type: DataTypes.STRING,
       allowNull: false
@@ -70,9 +70,9 @@ Array.prototype.remove = function() {
     return this;
 };
 
-async function whatsAsena () {
+async function Dada () {
     await config.DATABASE.sync();
-    var StrSes_Db = await WhatsAsenaDB.findAll({
+    var StrSes_Db = await DadaDB.findAll({
         where: {
           info: 'StringSession'
         }
@@ -99,7 +99,7 @@ async function whatsAsena () {
 
         const authInfo = conn.base64EncodedAuthInfo();
         if (StrSes_Db.length < 1) {
-            await WhatsAsenaDB.create({ info: "StringSession", value: Session.createStringSession(authInfo) });
+            await DadaDB.create({ info: "StringSession", value: Session.createStringSession(authInfo) });
         } else {
             await StrSes_Db[0].update({ value: Session.createStringSession(authInfo) });
         }
@@ -369,4 +369,4 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
     }
 }
 
-whatsAsena();
+Dada();
